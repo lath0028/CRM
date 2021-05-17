58ac883818fba28f737b40a34ca3296102b22779
LATH0028 <lath0028@algonquinlive.com>
2021-05-17 00:32:59 -0400

    Initial commit

---
+++ src/models/crmModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+export const ContactSchema = new Schema({
+    firstName: {
+        type: String,
+        required: 'Enter a first Name'
+    },
+    lastName: {
+        type: String,
+        required: 'Enter a last Name'
+    },
+    email: {
+        type: String,
+    },
+    company: {
+        type: String,
+
+    },
+    phoneNumber: {
+        type: Number
+
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+});
---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,33 @@
+
+import {
+    addNewContact,
+    getContact,
+    getContactWithID,
+    updateContact,
+    deleteContact
+} from '../controllers/crmController'
+const routes = (app) => {
+
+    app.route('/contact')
+        .get((req, res, next) => {
+            //middleware
+            console.log(`Request from: ${req.originalUrl}`)
+            console.log(`Request type: ${req.method}`)
+            next();
+        }, getContact)
+
+        //post endpoint
+        .post(addNewContact);
+
+    app.route('/contact/:contactID')
+
+        //get a specific contact
+        .get(getContactWithID)
+
+        // updating a specific contact
+        .put(updateContact)
+        //deleting a specific contact
+        .delete(deleteContact);
+}
+
+export default routes;
